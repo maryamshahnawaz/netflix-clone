14c0d164908d256e919a53615695faa2c6a9255f
maryam <maryum_shahnawaz@yahoo.com>
2021-06-14 18:23:22 -0400

    created app and connect with firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAd9RfSWXKrUeSXdrWBpDNgfGs972b2r7U",
+  authDomain: "netflix-original-e17ba.firebaseapp.com",
+  projectId: "netflix-original-e17ba",
+  storageBucket: "netflix-original-e17ba.appspot.com",
+  messagingSenderId: "165666321587",
+  appId: "1:165666321587:web:fd680e2a069eb99c1d6cc6"
+};
