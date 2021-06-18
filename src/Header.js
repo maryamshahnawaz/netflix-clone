fd9a29976ded41279b469ed3bfd9483338b0253e
maryam <maryum_shahnawaz@yahoo.com>
2021-06-18 18:19:00 -0400

    Styled header, made an api call that display’s popular movies on the header background and on front of the header add title, overview and movies rating in percentage.

--- src/Header.js
+++ src/Header.js
@@ -1,32 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import "./Header.css";
 
 const Header = () => {
+  const [background, setBackground] = useState('');
+
+  useEffect(() => {
+    axios({
+      url: 'https://api.themoviedb.org/3/movie/popular?',
+      method: 'GET',
+      dataResponse: 'json',
+      params: {
+        api_key: "7b4ee228270013c7be42d484778165ef",
+      },
+
+    }).then((res) => {
+      setBackground(res.data.results[Math.floor(Math.random() * res.data.results.length - 1)]);
+    })
+  }, []);
   const truncate = (str, num) => {
     return str?.length > num ? str.substr(0, num - 1) + "..." : str;
-
   }
+  console.log(background
+  );
+
   return (
-    <header className="movie
-    _header" style={{
-        backgroundImage: `url('https://help.nflxext.com/0af6ce3e-b27a-4722-a5f0-e32af4df3045_what_is_netflix_5_en.png')`,
+    <>
+      <header className="movie_header" style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${background?.backdrop_path}")`,
         backgroundPosition: 'center center',
-        height: "70vh"
+        backgroundSize: 'cover',
+        height: "85vh"
 
       }}>
-      <div className="header_content">
-        <h1>title</h1>
-        <p className="">{truncate(`Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ipsam distinctio eaque ad incidunt sint perferendis aliquam, officia numq impedit harum blanditiis or quis earum </p>`, 150)}</p>
-        <div className="header_btns">
-          <button>
-            Play
-          </button>
-          <button>
-            More Info
-          </button>
+
+        <div className="header_content">
+          <p class="popularity">{background?.vote_average * 10}%</p>
+          <h1>{background?.title || background?.name || background?.original_name}</h1>
+          <p className="">{truncate(`${background.overview}`, 150)}</p>
+          <div className="header_btns">
+            <button>
+              Play
+            </button>
+            <button>
+              More Info
+            </button>
+          </div>
         </div>
-      </div>
-    </header>
+      </header >
+    </>
   )
 }
 
